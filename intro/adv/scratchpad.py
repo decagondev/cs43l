2c5bee4daffa34ed5d8a2a01cb9eb8749e743175
Tom Tarpey <tomtarpeydev@gmail.com>
2021-05-04 14:51:04 +0100

    adv day 1 concepts

---
+++ intro/adv/scratchpad.py
@@ -0,0 +1,35 @@
+class Box:
+    def __init__(self, name):
+        self.name = name
+
+    
+class Person:
+    def __init__(self, name, current_box):
+        self.name = name
+        self.current_box = current_box
+
+box1 = Box("box1")
+box2 = Box("box2")
+box3 = Box("box3")
+box4 = Box("box4")
+
+box1.n_box = box3
+box3.s_box = box1
+box3.w_box = box4
+box4.e_box = box3
+box4.n_box = box2
+box2.s_box = box4
+
+person = Person("Dave", box1)
+dir = "n"
+attrib = dir + "_box"
+
+# current_box = box4
+print(f"{person.name} is in {person.current_box.name}") # => box 4
+if hasattr(person.current_box, attrib):
+    person.current_box = getattr(person.current_box, attrib) # person.current_box.n_box
+else:
+    print(f"{person.name} can not go that way!")
+
+print(f"{person.name} is in {person.current_box.name}") # => box 2
+    
