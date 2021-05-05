9dd9f7fdecf2700e9cdb46f87a7e199c5c3cc2d9
Tom Tarpey <tomtarpeydev@gmail.com>
2021-05-05 19:16:06 +0100

    end of graphs connected components

---
+++ graphs/islands.py
@@ -0,0 +1,100 @@
+# Write a function that takes a 2D binary array and returns the number of 1 islands. 
+# An island consists of 1s that are connected to the north, south, east or west. 
+# For example:
+
+def get_neighbors(row, col, graph_matrix):
+    neighbors = []
+    # Check north
+    if row > 0 and graph_matrix[row-1][col] == 1:
+        neighbors.append((row-1, col))
+    # Check south
+    if row < len(graph_matrix) - 1 and graph_matrix[row+1][col] == 1:
+        neighbors.append((row+1, col))
+    # Check east
+    if col < len(graph_matrix[0]) - 1 and graph_matrix[row][col+1] == 1:
+        neighbors.append((row, col+1))
+    # Check west
+    if col > 0 and graph_matrix[row][col-1] == 1:
+        neighbors.append((row, col-1))
+    # Return all directions that contain a 1
+    return neighbors
+
+class Stack:
+    def __init__(self):
+        self.storage = []
+    
+    def push(self, value):
+        self.storage.append(value)
+    
+    def pop(self):
+        if self.size() > 0:
+            return self.storage.pop()
+        else:
+            return None
+    
+    def size(self):
+        return len(self.storage)
+    
+def dft(row, col, islands, visited):
+    s = Stack()
+
+    s.push((row, col))
+
+    while s.size() > 0:
+        row, col = s.pop()
+        # node = s.pop()
+        # row = node[0]
+        # col = node[1]
+        if not visited[row][col]:
+            visited[row][col] = True
+
+            for neighbor in get_neighbors(row, col, islands):
+                s.push(neighbor)
+    
+    return visited
+
+
+def island_counter(islands):
+    counter = 0
+    visited = []
+    for i in range(len(islands)):
+        visited.append([False] * len(islands[0]))
+    
+    for col in range(len(islands[0])):
+        for row in range(len(islands)):
+            if not visited[row][col]:
+                if islands[row][col] == 1:
+                    visited = dft(row, col, islands, visited)
+                    counter += 1
+                else:
+                    visited[row][col] = True
+    
+    return counter
+
+
+
+islands = [[0, 1, 0, 1, 0],
+           [1, 1, 0, 1, 1],
+           [0, 0, 1, 0, 0],
+           [1, 0, 1, 0, 0],
+           [1, 1, 0, 0, 0]]
+
+print(island_counter(islands)) # returns 4
+
+# islands = [[1, 0, 0, 1, 1, 0, 1, 1, 0, 1],
+#            [0, 0, 1, 1, 0, 1, 0, 0, 0, 0],
+#            [0, 1, 1, 1, 0, 0, 0, 1, 0, 1],
+#            [0, 0, 1, 0, 0, 1, 0, 0, 1, 1],
+#            [0, 0, 1, 1, 0, 1, 0, 1, 1, 0],
+#            [0, 1, 0, 1, 1, 1, 0, 1, 0, 0],
+#            [0, 0, 1, 0, 0, 1, 1, 0, 0, 0],
+#            [1, 0, 1, 1, 0, 0, 0, 1, 1, 0],
+#            [0, 1, 1, 0, 0, 0, 1, 1, 0, 0],
+#            [0, 0, 1, 1, 0, 1, 0, 0, 1, 0]]
+
+# print(island_counter(islands))  # 13
+
+# islands = [[1, 0, 0, 1, 1, 0, 1, 1, 0, 1]] # 4
+
+# print(island_counter(islands))
+
