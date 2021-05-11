62de0098c7cf313da161a532c28ff572014b9d29
Tom Tarpey <tomtarpeydev@gmail.com>
2021-05-11 19:24:07 +0100

    simple.py update for day 2 un hardcoded program

--- arch/simple.py
+++ arch/simple.py
@@ -1,3 +1,4 @@
+import sys
 HALT = 1
 PRINT_BOB = 2
 PRINT_NUM = 3
@@ -5,6 +6,11 @@ STORE = 4
 PRINT_REG = 5
 LDI = 0b10000010
 HLT = 0b00000001
+
+
+def alu(op, opa, opb):
+    if op == "ADD":
+        reg[opa] += reg[opb]
 
 ram = [0] * 256
 
@@ -20,6 +26,26 @@ ram[6] = 120
 ram[7] = PRINT_REG
 ram[8] = 0
 ram[9] = HALT
+
+def load(filename):
+    with open(filename) as filedata:
+        addr = 0
+        for line in filedata:
+            
+            data = line.split('#')[0].strip()
+            if data == "":
+                continue
+
+            num = int(data)
+            ram[addr] = num
+            # self.ram_write(addr, num)
+            addr += 1
+        
+if len(sys.argv) != 2:
+    print(f"Usage: simple.py <filename>")
+    sys.exit(1)
+    
+load(sys.argv[1])
 
 pc = 0
 
