23b7a4c7b6f0e2ba135b0bc8f1e7dcce4e7f0029
Tom Tarpey <tomtarpeydev@gmail.com>
2021-05-12 19:55:22 +0100

    Update simple.py

--- arch/simple.py
+++ arch/simple.py
@@ -4,6 +4,8 @@ PRINT_BOB = 2
 PRINT_NUM = 3
 STORE = 4
 PRINT_REG = 5
+PUSH = 6
+POP = 7
 LDI = 0b10000010
 HLT = 0b00000001
 
@@ -14,7 +16,11 @@ def alu(op, opa, opb):
 
 ram = [0] * 256
 
+sp = 7
+
 reg = [0] * 8
+reg[sp] = 0xf4
+
 # our program
 ram[0] = PRINT_BOB
 ram[1] = PRINT_BOB
@@ -25,7 +31,18 @@ ram[5] = 0
 ram[6] = 120
 ram[7] = PRINT_REG
 ram[8] = 0
-ram[9] = HALT
+ram[9] = PUSH
+ram[10] = 0
+ram[11] = STORE
+ram[12] = 0
+ram[13] = 12
+ram[14] = PRINT_REG
+ram[15] = 0
+ram[16] = POP
+ram[17] = 0
+ram[18] = PRINT_REG
+ram[19] = 0
+ram[20] = HALT
 
 def load(filename):
     with open(filename) as filedata:
@@ -41,11 +58,11 @@ def load(filename):
             # self.ram_write(addr, num)
             addr += 1
         
-if len(sys.argv) != 2:
-    print(f"Usage: simple.py <filename>")
-    sys.exit(1)
+# if len(sys.argv) != 2:
+#     print(f"Usage: simple.py <filename>")
+#     sys.exit(1)
     
-load(sys.argv[1])
+# load(sys.argv[1])
 
 pc = 0
 
@@ -71,4 +88,25 @@ while running:
     elif inst == PRINT_REG:
         reg_index = ram[pc + 1]
         print(reg[reg_index])
+        pc += 2
+    elif inst == PUSH:
+        # dec the SP
+        reg[sp] -= 1
+        # grab the index to the reg
+        reg_index = ram[pc + 1]
+
+        #get the value from the reg
+        value = reg[reg_index]
+        ram[reg[sp]] = value
+        pc += 2
+
+    elif inst == POP:
+        # grab the index to the reg
+        reg_index = ram[pc + 1]
+        # get the value from the top of the stack
+        value = ram[reg[sp]]
+        # inc my stack pointer
+        reg[sp] += 1
+        reg[reg_index] = value
+
         pc += 2
